ce4a3e7e61292fb11fe5b790813b0f196a0be2a8
Makcim80 <makcim80@mail.ru>
2023-01-12 17:38:14 +0300

    feat: implement task in src/index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
+  let result = ''
+
+  n = Math.abs(n).toString()
+
+  for (let i = 0; i < n.length; i++) {
+    result = n[i] + result
+  }
   
+  return +result
 }
